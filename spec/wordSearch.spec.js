87636b2f98280ef26bf3adc45ea2ff3c097745f8
Yuriy Pliskovsky <zzaafamania@gmail.com>
2022-10-04 19:00:37 +0300

    version 1.0

---
+++ spec/wordSearch.spec.js
@@ -0,0 +1,21 @@
+describe ('searchStraightLineInclude', () => {
+  it('', () => {
+    expect()
+  })
+
+  it('', () => {
+    expect()
+  })
+})
+
+
+describe ('searchSnakingInclude', () => {
+  it('', () => {
+    expect()
+  })
+
+  it('', () => {
+    expect()
+  })
+})
+
