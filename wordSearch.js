da1847f30a3249c0ce02dd78710e2bdb92919fc7
skorobogatovamail <skorobogatovamail@gmail.com>
2024-04-25 19:26:13 +0300

    release 1

--- wordSearch.js
+++ wordSearch.js
@@ -1,11 +1,11 @@
 let puzzle = [
-  ["a", "k", "f", "o", "x", "e", "s"],
-  ["s", "o", "a", "w", "a", "h", "p"],
-  ["i", "t", "c", "k", "e", "t", "n"],
-  ["o", "t", "s", "d", "h", "o", "h"],
-  ["s", "e", "x", "g", "s", "t", "a"],
-  ["u", "r", "p", "i", "w", "e", "u"],
-  ["z", "s", "b", "n", "u", "i", "r"],
+  ['a', 'k', 'f', 'o', 'x', 'e', 's'],
+  ['s', 'o', 'a', 'w', 'a', 'h', 'p'],
+  ['i', 't', 'c', 'k', 'e', 't', 'n'],
+  ['o', 't', 's', 'd', 'h', 'o', 'h'],
+  ['s', 'e', 'x', 'g', 's', 't', 'a'],
+  ['u', 'r', 'p', 'i', 'w', 'e', 'u'],
+  ['z', 's', 'b', 'n', 'u', 'i', 'r'],
 ];
 
 function searchStraightLineInclude(word, puzzle) {
@@ -14,13 +14,13 @@ function searchStraightLineInclude(word, puzzle) {
   // test regular expression with given word in concatenated string
 
   function testWord(arr) {
-    const words = arr.map((el) => el.join(""));
-    const wordsReverse = arr.map((el) => el.reverse().join(""));
+    const words = arr.map((el) => el.join(''));
+    const wordsReverse = arr.map((el) => el.reverse().join(''));
 
     const regex = new RegExp(word);
     const bools = words.map((el) => regex.test(el));
     const boolsReverse = wordsReverse.map((el) => regex.test(el));
-    return bools.some((e) => e) || boolsReverse.some((e) => e);
+    return bools.some((e) => e === true) || boolsReverse.some((e) => e);
   }
 
   // transpose matrix
@@ -59,7 +59,7 @@ function searchStraightLineInclude(word, puzzle) {
       const numObjectsInDiagonal = Math.min(
         diagonalLineNum,
         nCols - startCol,
-        nRows
+        nRows,
       );
 
       // print elements of diagonal
@@ -77,16 +77,77 @@ function searchStraightLineInclude(word, puzzle) {
 
   const puzzleTransposed = transposeMatrix(puzzle);
   const puzzleDiagonal = getDiagonals(puzzle);
-//   console.log(puzzleDiagonal);
+  //   console.log(puzzleDiagonal);
 
   return (
     testWord(puzzle) || testWord(puzzleTransposed) || testWord(puzzleDiagonal)
   );
 }
 
-function searchSnakingInclude(word, puzzle) {}
+// console.log(searchStraightLineInclude('foxes', puzzle));
+// console.log(searchStraightLineInclude('otters', puzzle));
+// console.log(searchStraightLineInclude('bison', puzzle));
+// console.log(searchStraightLineInclude('cat', puzzle));
 
-console.log(searchStraightLineInclude("foxes", puzzle));
-console.log(searchStraightLineInclude("otters", puzzle));
-console.log(searchStraightLineInclude("bison", puzzle));
-console.log(searchStraightLineInclude("cat", puzzle));
+function searchSnakingInclude(word, puzzle) {
+  // get all indexes of element in matrix
+  // console.log(getAllIndexes('f', puzzle));
+  function getAllIndexes(letter, matrix) {
+    const letterIndexes = [];
+    for (let i = 0; i < matrix.length; i += 1) {
+      for (let j = 0; j < matrix[0].length; j += 1) {
+        if (matrix[i][j] === letter) {
+          letterIndexes.push([i, j]);
+        }
+      }
+    }
+    return letterIndexes;
+  }
+  function compareIndexes(coords1, coords2) {
+    const row1 = coords1[0];
+    const col1 = coords1[1];
+
+    const row2 = coords2[0];
+    const col2 = coords2[1];
+    if (Math.abs(row1 - row2) < 2 && Math.abs(col1 - col2) < 2) {
+      return true;
+    }
+    return false;
+  }
+
+  // 1 collect indexes (coordinates) of all letters
+  //   {
+  //     f: [ [ 0, 2 ] ],
+  //     o: [ [ 0, 3 ], [ 1, 1 ], [ 3, 0 ], [ 3, 5 ] ],
+  //     x: [ [ 0, 4 ], [ 4, 2 ] ]
+  //   }
+  const letters = word.split('');
+  const coordinates = {};
+  for (let i = 0; i < letters.length; i += 1) {
+    coordinates[letters[i]] = getAllIndexes(letters[i], puzzle);
+  }
+
+  // 2 for every letter calculate distance to next letter
+  // row1 - row2 < 2 && col1 - col2
+  // return coordinates;
+  for (let i = 0; i < letters.length - 1; i += 1) {
+    const curLetter = letters[i];
+    const nextLetter = letters[i + 1];
+    const curLetterIdxs = coordinates[curLetter];
+    const nextLetterIdxs = coordinates[nextLetter];
+
+    const comparisons = [];
+    for (let i = 0; i < curLetterIdxs.length; i += 1) {
+      for (let j = 0; j < nextLetterIdxs.length; j += 1) {
+        comparisons.push(compareIndexes(curLetterIdxs[i], nextLetterIdxs[j]));
+      }
+    }
+    if (comparisons.every((el) => el === false)) return false;
+  }
+  return true;
+}
+
+console.log(searchSnakingInclude('akotters', puzzle)); // true
+console.log(searchSnakingInclude('weirua', puzzle)); // true
+console.log(searchSnakingInclude('afx', puzzle)); // false
+console.log(searchSnakingInclude('ophp', puzzle)); // false
